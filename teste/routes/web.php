49955fe964f4293c6bd80c1b453fae59de0ce203
hchimachi <hchimachi@gmail.com>
2021-08-20 19:57:37 -0300

    001

--- teste/routes/web.php
+++ teste/routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/teste', function () {
+    return view('teste');
+});
 
 
 Route::group(['prefix' => 'admin'], function () {
